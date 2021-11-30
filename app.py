fe004b101df424404deab4a26951a7a62a44f87b
iRaySpace <irayspacii@gmail.com>
2021-11-30 09:25:52 +0800

    feat: added get_normal_forms_from_sentence

--- app.py
+++ app.py
@@ -7,3 +7,8 @@ def get_normal_forms(word):
     parsed_word = morph.parse(word)
     normal_forms = [x.normal_form for x in parsed_word]
     return list(set(normal_forms))
+
+
+def get_normal_forms_from_sentence(sentence):
+    words = sentence.split(' ')
+    return {x: get_normal_forms(x) for x in words}
